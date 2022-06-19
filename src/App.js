e5b034b5421c7db908a920f4d39ebcdef987d441
sbkcoder <simonbknight@gmail.com>
2022-06-19 13:49:12 -0500

    adding first button with animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import { motion } from 'framer-motion';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <motion.button
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
+       className="save-button"
+       onClick={() => null}
       
+      >
+      Launch Modal
+      </motion.button>
     </div>
   );
 }
